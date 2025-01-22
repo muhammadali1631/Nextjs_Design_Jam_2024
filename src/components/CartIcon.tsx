156a4a74d291d44fc352c8a9d508a94e5e92296e
Ali Shahzad <160423266+muhammadali1631@users.noreply.github.com>
2025-01-22 14:49:31 +0500

    cart done

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { AiOutlineShopping } from "react-icons/ai";
 import { TbShoppingBagCheck } from "react-icons/tb";
 
 interface Props {
