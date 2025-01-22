347e415bcc23f9eb2d8fed0dde9ce07ac690bc5d
Ali Shahzad <160423266+muhammadali1631@users.noreply.github.com>
2025-01-22 12:35:32 +0500

    done

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher(['/orders', '/submitorder'])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) await auth.protect()
+  if (isPublicRoute(req)) await auth.protect()
 })
 
 export const config = {
