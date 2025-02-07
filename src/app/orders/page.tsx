6a0f21844ee83135ef788ff30d2635e94742dc13
Ali Shahzad <160423266+muhammadali1631@users.noreply.github.com>
2025-02-07 15:33:37 +0500

    done

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -36,7 +36,7 @@ const Page = () => {
                   {item.status}
                 </span>
               </td>
-              <td className='border border-gray-300 px-4 py-2'>{Data.products.length}</td>
+              <td className='border border-gray-300 px-4 py-2'>{item.products.length}</td>
             </tr>
           ))}
         </tbody>
